b001b6ef7b8f633e6e5ed5fd3cdc38b8d95e6402
Gus Doriguetto <doriguetto@adobe.com>
2025-01-21 17:45:16 +1100

    test

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,2 +1,2 @@
-import{d}from"../chunkz/header.DFf15R0h.js";import"../chunkz/render.BNIA9sCb.js";import"../chunkz/html.CvX0OcDZ.js";import"../chunkz/if.D0Gwi6K8.js";import"../chunkz/class.RfztAWKp.js";import"../chunkz/chevron-right.BIZwU9QK.js";export{d as default};
+import{d}from"../__chunks__/header.DFf15R0h.js";import"../__chunks__/render.BNIA9sCb.js";import"../__chunks__/html.CvX0OcDZ.js";import"../__chunks__/if.D0Gwi6K8.js";import"../__chunks__/class.RfztAWKp.js";import"../__chunks__/chevron-right.BIZwU9QK.js";export{d as default};
 //# sourceMappingURL=header.js.map
--- vite.config.ts
+++ vite.config.ts
@@ -123,7 +123,7 @@ export default defineConfig((configEnv) => {
                     //     }
                     //     return id
                     // },
-                    chunkFileNames: 'chunkz/[name].[hash].js',
+                    chunkFileNames: '__chunks__/[name].[hash].js',
                     entryFileNames: '[name]/[name].js',
                 },
 
