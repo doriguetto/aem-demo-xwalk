a860adb36a5bb58e36f1189d0c7b7b0d74a19c7f
Gus Doriguetto <doriguetto@adobe.com>
2025-01-24 11:13:56 +1100

    test

--- blocks/chunk.legacy.Cmh0AMAJ.js
+++
@@ -1,2 +0,0 @@
-import{aG as a}from"./chunk.html.CvX0OcDZ.js";a();
-//# sourceMappingURL=chunk.legacy.Cmh0AMAJ.js.map
--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,2 +1,2 @@
-import{d}from"../chunk.header.BfTcEY5P.js";import"../chunk.render.PvBLG7Ww.js";import"../chunk.html.CvX0OcDZ.js";import"../chunk.if.C72ifDWu.js";import"../chunk.class.D7hUsr8n.js";import"../chunk.chevron-right.BIZwU9QK.js";export{d as default};
+import{d}from"../chunk/header.DFf15R0h.js";import"../chunk/render.BNIA9sCb.js";import"../chunk/html.CvX0OcDZ.js";import"../chunk/if.D0Gwi6K8.js";import"../chunk/class.RfztAWKp.js";import"../chunk/chevron-right.BIZwU9QK.js";export{d as default};
 //# sourceMappingURL=header.js.map
